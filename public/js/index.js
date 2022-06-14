6288c743fe34457c33dd98626d8e8b7197d368d8
1deme <demetrelabadze1@gmail.com>
2022-06-14 23:29:47 +0400

    add files

---
+++ public/js/index.js
@@ -0,0 +1,46 @@
+var nowShowing = 49;
+var searchInput = document.getelementById('searchbar');
+
+function loadMore(){
+    for(var i=nowShowing; i<nowShowing+21;i++){
+            document.getElementById(i).classList.remove('card2');
+            document.getElementById(i).classList.add('card');
+    }
+    nowShowing+=21;
+}
+
+searchInput.addEventListener('keyup', function(e){
+    const value = e.target.value.toLowerCase();
+    const cards = document.getElementsByClassName('card');
+});
+
+function levenshtein(a, b) {
+    if(a.length == 0) return b.length; 
+    if(b.length == 0) return a.length; 
+
+    var matrix = [];
+
+    var i;
+    for(i = 0; i <= b.length; i++){
+        matrix[i] = [i];
+    }
+
+    var j;
+    for(j = 0; j <= a.length; j++){
+        matrix[0][j] = j;
+    }
+
+    for(i = 1; i <= b.length; i++){
+        for(j = 1; j <= a.length; j++){
+            if(b.charAt(i-1) == a.charAt(j-1)){
+                matrix[i][j] = matrix[i-1][j-1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i-1][j-1] + 1,
+                                    Math.min(matrix[i][j-1] + 1, 
+                                    matrix[i-1][j] + 1)); 
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
