a60b6ae9cf5adcdacde67ffe9485b09e5ae963ce
1deme <demetrelabadze1@gmail.com>
2022-06-16 14:25:07 +0400

    move pure javascript in public folder

--- public/js/index.js
+++ public/js/index.js
@@ -1,46 +1,103 @@
-var nowShowing = 49;
-var searchInput = document.getelementById('searchbar');
+    var searchInput =  document.getElementById("searchbar"); 
+    var cards = document.querySelectorAll(".card,.card2");
 
-function loadMore(){
-    for(var i=nowShowing; i<nowShowing+21;i++){
-            document.getElementById(i).classList.remove('card2');
-            document.getElementById(i).classList.add('card');
+    searchInput.addEventListener('keyup', function(e){
+      const value = e.target.value.toLowerCase().replace(/ /g,'');
+      cards.forEach(card =>{
+        const modCard = 
+          card.querySelector("h3").innerHTML.toLowerCase().replace(/ /g,'')
+          + card.querySelector("h1").innerHTML.toLowerCase().replace(/ /g,'');
+        
+        console.log(modCard);
+        const price = card.querySelector("price").innerHTML;
+        var l = check(modCard,value);
+        var l2 =  checkPriceBounds(price,value);
+        if(l && l2 ){
+          card.style.display = "block";
+        }
+        else{
+          card.style.display = "none";
+        }
+
+      })
+    });
+    
+    function check(modCard,value){
+      var valueLength = value.length;
+      var index = value.lastIndexOf(":");
+      var names = value.substring(0,!(index+1)?valueLength:(index));
+      var price = value;
+      var namesArray = names.split(",");
+      var ans = false;
+      namesArray.forEach(name =>{
+        if(modCard.includes(name) || (levenshtein(name,modCard)<modCard.length/3) ){
+          ans = true;
+        }
+      })
+      return ans;
     }
-    nowShowing+=21;
+
+    
+    function checkPriceBounds(price,boundString){
+    var start = boundString.indexOf("(");
+    var end = boundString.indexOf(")");
+    if(start==-1 && end==-1 && start>end ){
+        return true; 
+    }
+    var parsedboundString = boundString.substring(start+1,end)
+    var lowBound = parsedboundString.substring(0,parsedboundString.indexOf("-"));
+    var highBound = parsedboundString.substring(parsedboundString.indexOf("-")+1,parsedboundString.length);
+    console.log(isNaN(lowBound)+" "+ isNaN(highBound)+" "+isNaN(price)+" "+lowBound+" "+highBound+" "+price);
+    if(highBound==""){
+      highBound = Number.MAX_SAFE_INTEGER;
+    }
+    if(lowBound==""){
+        lowBound = 0;
+    }
+    if(isNaN(lowBound) && isNaN(highBound)){
+        return true;
+    }
+    if(isNaN(lowBound) && !isNaN(highBound) && parseInt(price,10)<=parseInt(highBound,10) ){
+        return true;    
+    }
+    if(!isNaN(lowBound) && isNaN(highBound) && parseInt(price,10)>=parseInt(lowBound,10) ){
+        return true;
+    }
+    console.log((price<=highBound)+" "+price+" "+highBound+"esaa essss");
+    if(!isNaN(lowBound) && !isNaN(highBound) 
+    && parseInt(price,10)>=parseInt(lowBound,10) && parseInt(price,10)<=parseInt(highBound,10)){
+        return true;
+    }
+    return false;
 }
+  
 
-searchInput.addEventListener('keyup', function(e){
-    const value = e.target.value.toLowerCase();
-    const cards = document.getElementsByClassName('card');
-});
+    function levenshtein(a, b) {
+        if(a.length == 0) return b.length; 
+        if(b.length == 0) return a.length; 
 
-function levenshtein(a, b) {
-    if(a.length == 0) return b.length; 
-    if(b.length == 0) return a.length; 
+        var matrix = [];
 
-    var matrix = [];
+        var i;
+        for(i = 0; i <= b.length; i++){
+            matrix[i] = [i];
+        }
 
-    var i;
-    for(i = 0; i <= b.length; i++){
-        matrix[i] = [i];
-    }
+        var j;
+        for(j = 0; j <= a.length; j++){
+            matrix[0][j] = j;
+        }
 
-    var j;
-    for(j = 0; j <= a.length; j++){
-        matrix[0][j] = j;
-    }
-
-    for(i = 1; i <= b.length; i++){
-        for(j = 1; j <= a.length; j++){
-            if(b.charAt(i-1) == a.charAt(j-1)){
-                matrix[i][j] = matrix[i-1][j-1];
-            } else {
-                matrix[i][j] = Math.min(matrix[i-1][j-1] + 1,
-                                    Math.min(matrix[i][j-1] + 1, 
-                                    matrix[i-1][j] + 1)); 
+        for(i = 1; i <= b.length; i++){
+            for(j = 1; j <= a.length; j++){
+                if(b.charAt(i-1) == a.charAt(j-1)){
+                    matrix[i][j] = matrix[i-1][j-1];
+                } else {
+                    matrix[i][j] = Math.min(matrix[i-1][j-1] + 1,
+                                        Math.min(matrix[i][j-1] + 1, 
+                                        matrix[i-1][j] + 1)); 
+                }
             }
         }
+        return matrix[b.length][a.length];
     }
-
-    return matrix[b.length][a.length];
-}
