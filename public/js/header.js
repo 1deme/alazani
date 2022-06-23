b222bd4be6d7024e5dd6d999a5d81fd0f28621df
1deme <demetrelabadze1@gmail.com>
2022-06-23 22:01:11 +0400

    add focus in searchbar onload

--- public/js/header.js
+++ public/js/header.js
@@ -1,3 +1,9 @@
+window.onload = getFocus();
+
+function getFocus() {
+    document.getElementById("searchbar").focus();
+}
+
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
