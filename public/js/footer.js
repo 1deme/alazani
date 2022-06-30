7ea47a8030d8cf777d177d3c5d81127ecf430a65
1deme <demetrelabadze1@gmail.com>
2022-06-30 19:23:21 +0400

    add jsdoc comment in footer.js

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,6 +1,8 @@
+/**
+ * redirects web to argument email
+ * @param {string} EmailAddress - addres of email web will be redirected to.
+ */
 function sentEmail(EmailAddress){
     var linkArray = ['https://mail.google.com/mail/u/0/?fs=1&to=', EmailAddress, '&tf=cm'];
     window.location.href = linkArray.join('');
 }
-
-// https://mail.google.com/mail/u/0/?fs=1&to=email@domain.example&tf=cm -->
